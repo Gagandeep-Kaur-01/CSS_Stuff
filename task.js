9083f93f41e2b4d162ddfd5c0f6e8eee255ba913
Gagandeep07-kaur <gagandeep07kaur@gmail.com>
2019-01-14 20:22:28 +0530

    12 jan task

---
+++ task.js
@@ -0,0 +1,7 @@
+
+var cap = ["Chandigarh", "Patna", "Dispur", "Chandigarh", "Raipur"];
+var stat =  ["Punjab", "Bihar", "Assam", "Haryana", "Chhattisgarh"];
+
+
+
+document.getElementById("capital").innerHTML = (Math.floor(Math.random() * cap.length));
